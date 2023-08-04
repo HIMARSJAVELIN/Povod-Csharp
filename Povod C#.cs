e150d5e8675969e04c92b38a55bba933258a81c2
HIMARSJAVELIN <141017802+HIMARSJAVELIN@users.noreply.github.com>
2023-08-04 20:13:41 +0300

    Update Povod C#.cs

--- Povod C#.cs
+++ Povod C#.cs
@@ -47,7 +47,7 @@ internal class Program
 
         for (int i = 0; i < myArray.Length; i++)
         {
-            Console.Write($"\nВведите целое число от 0 до 100 - {i + 1}-й элемент массива: ");
+            Console.Write($"\nВведите целое число от 1 до 100 - {i + 1}-й элемент массива: ");
             myArray[i] = int.Parse(Console.ReadLine());
         }
 
